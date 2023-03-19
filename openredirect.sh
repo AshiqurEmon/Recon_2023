a689602ddc748197a1ad44e6a321225d092a1df0
AshiqurEmon <45551479+AshiqurEmon@users.noreply.github.com>
2023-03-19 11:19:41 +0600

    Update openredirect.sh

--- openredirect.sh
+++ openredirect.sh
@@ -4,8 +4,8 @@ domain=$1
 
 url=$2
 
-cat $url |while read d;do cat $1|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew|tee -a urls.txt;done
+cat $url |while read d;do cat $1|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew>> urls.txt;done
 
-cat $url |while read d;do cat $1|grep '='| qsreplace $d|anew|tee -a urls.txt;done
+cat $url |while read d;do cat $1|grep '='| qsreplace $d|anew>> urls.txt;done
 
 cat urls.txt | httpx -location | tee -a redirect_vul_url.txt
