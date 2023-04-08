3b163d9e66232469c75ea837e628229469486a78
AshiqurEmon <45551479+AshiqurEmon@users.noreply.github.com>
2023-04-08 12:58:19 +0600

    Update openredirect.sh

--- openredirect.sh
+++ openredirect.sh
@@ -3,9 +3,13 @@
 domain=$1
 
 url=$2
+onlydomain=$3
 
 cat $url |while read d;do cat $1|uro|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew>> urls.txt;done
 
 cat $url |while read d;do cat $1|uro|grep '='| qsreplace $d|anew>> urls.txt;done
 
-cat urls.txt | httpx -location -x all| tee -a redirect_vul_url.txt
+cat urls.txt | httpx -location -x all
+cat $3 |while read d;do echo $d'/'|tee -a url_slashed.txt;done
+cat url_slashed.txt| httpx -mr 'webhook' -silent -x all -path $2 
+rm url_slashed.txt
