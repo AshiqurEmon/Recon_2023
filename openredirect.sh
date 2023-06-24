26147ddf8ced174c3afccced37ee582949b413a0
AshiqurEmon <45551479+AshiqurEmon@users.noreply.github.com>
2023-06-24 16:13:38 +0600

    Update openredirect.sh

--- openredirect.sh
+++ openredirect.sh
@@ -5,9 +5,9 @@ domain=$1
 url=$2
 onlydomain=$3
 
-cat $url |while read d;do cat $1|uro|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew>> urls.txt;done
+cat $url |while read d;do cat $1|anew|grep '='| qsreplace $d|python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"|grep $d|anew>> urls.txt;done
 
-cat $url |while read d;do cat $1|uro|grep '='| qsreplace $d|anew>> urls.txt;done
+cat $url |while read d;do cat $1|anew|grep '='| qsreplace $d|anew>> urls.txt;done
 
 cat urls.txt | httpx -location -x all
 cat $3 |while read d;do echo $d'/'|tee -a url_slashed.txt;done
